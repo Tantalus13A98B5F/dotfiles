1a0bd35765021bfdacad02ec33adc371a2ed3836
Tantalus <jsl_713@live.com>
2021-06-15 10:48:24 +0800

    split gitconf.sh

---
+++ gitconf.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+GIT=${1:-git}
+read -p "(git) Name: " temp
+$GIT config --global user.name $temp
+read -p "(git) Email: " temp
+$GIT config --global user.email $temp
+
+$GIT config --global alias.chmod "update-index --chmod"
+$GIT config --global alias.show-mode "ls-tree -rt HEAD"
+$GIT config --global alias.assume-unchanged "update-index --assume-unchanged"
+$GIT config --global alias.show-unchanged "ls-files -v"
+
+GITEXE="$(which git.exe)"
+if [ "$GIT" != "git.exe" ] && [ -n "$GITEXE" ] && [ "$GITEXE" != "$(which git).exe" ]; then
+    GfWPath="$(dirname "$(dirname "$GITEXE")")"
+    GCMCore="$GfWPath/mingw64/libexec/git-core/git-credential-manager.exe"
+    $GIT config --global credential.helper "${GCMCore/ /\\ }"
+fi
--- install.sh
+++ install.sh
@@ -4,25 +4,11 @@ cd "$HOME"
 
 ln -s "$DOTF/vimrc" .vimrc
 ln -s "$DOTF/tmux.conf" .tmux.conf
-if [ -n "$(which wsl.exe)" ]; then
+if [ -n "$(uname -r | grep microsoft)" ]; then
     ln -s "$DOTF/bash_aliases.wsl" .bash_aliases
 fi
 
-read -p "(git) Name: " temp
-git config --global user.name $temp
-read -p "(git) Email: " temp
-git config --global user.email $temp
-
-git config --global alias.chmod "update-index --chmod"
-git config --global alias.show-mode "ls-tree -rt HEAD"
-git config --global alias.assume-unchanged "update-index --assume-unchanged"
-git config --global alias.show-unchanged "ls-files -v"
-
-if [ -n "$(which git.exe)" ] && [ "$(which git.exe)" != "$(which git).exe" ]; then
-    GfWPath="$(dirname "$(dirname "$(which git.exe)")")"
-    GCMCore="$GfWPath/mingw64/libexec/git-core/git-credential-manager.exe"
-    git config --global credential.helper "${GCMCore/ /\\ }"
-fi
+./gitconf.sh
 
 echo 'Configured: vimrc, tmux.conf, bash_aliases.wsl, gitconfig;'
 echo 'Set up your proxy and open `vim` to continue.'
